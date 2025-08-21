d9304c2a50a02d0a36dcf28943adf1dc69fb3b46
Aidar Darmesh <darmesh.aidar@gmail.com>
2025-08-21 17:27:20 +0500

    Set up new Azure Function App

---
+++ function_app.py
@@ -0,0 +1,35 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.function_name(name="HttpTrigger")
+@app.route(route="http_trigger")
+@app.cosmos_db_output(
+    arg_name="outputDocument", 
+    database_name="my-database", 
+    container_name="my-container", 
+    connection="CosmosDbConnectionString")
+def http_trigger(
+    req: func.HttpRequest,
+    outputDocument: func.Out[func.Document]) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python Cosmos DB trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        outputDocument.set(func.Document.from_dict({"name": name}))
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
