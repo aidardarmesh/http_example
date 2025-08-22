bddc0cdf15aa857936a53a914bc4e0fa893c9b06
Aidar Darmesh <darmesh.aidar@gmail.com>
2025-08-22 15:56:13 +0500

    Removed CosmosDB Outputs

--- function_app.py
+++ function_app.py
@@ -6,20 +6,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.function_name(name="HttpTrigger")
 @app.route(route="http_trigger")
-@app.cosmos_db_output(
-    arg_name="outputDocument", 
-    database_name="my-database", 
-    container_name="my-container", 
-    connection="CosmosDB")
 def http_trigger(
-    req: func.HttpRequest,
-    outputDocument: func.Out[func.Document]
+    req: func.HttpRequest
 ) -> func.HttpResponse:
-    import os  
-    logging.info(f"CosmosDB string: {os.getenv('CosmosDB')}")
-    logging.info('Python HTTP trigger function processed a request.')
-    logging.info('Python Cosmos DB trigger function processed a request.')
-
     name = req.params.get('name')
     if not name:
         try:
@@ -30,7 +19,6 @@ def http_trigger(
             name = req_body.get('name')
 
     if name:
-        outputDocument.set(func.Document.from_dict({"id": name}))
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
